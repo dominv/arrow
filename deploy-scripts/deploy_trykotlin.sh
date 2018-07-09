a4654684ed140d746c51ad5d1fc32a69a9ec2a73
domlopez <domlopez@starbucks.com>
2018-07-09 12:41:09 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -10,6 +10,8 @@ git commit -m "Upgrading arrow version in trykotlin"
 
 # Push built subtree to trykotlinwebdemo
 git subtree push -P .trykotlin https://github.com/dominv/kotlin-web-demo.git master
+
+# Only needed in local to remove .trykotlin files. Not needed in arrowkt as long as this script will be run in travis
 rm -rf .trykotlin
 git add .trykotlin/
 git commit -m "Cleaning trykotlin"
